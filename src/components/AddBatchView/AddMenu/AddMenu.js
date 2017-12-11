63776f665bae82f022fba1a05b9962ecf3f802a5
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2017-12-11 20:53:56 +0100

    Add data_batches json

--- src/components/AddBatchView/AddMenu/AddMenu.js
+++ src/components/AddBatchView/AddMenu/AddMenu.js
@@ -1,3 +1,14 @@
 
 
+// Getting a reference to the database service
 const database = firebase.database();
+
+/*
+ function writeUserData(userId, name, email, imageUrl) {
+ firebase.database().ref('users/' + userId).set({
+ username: name,
+ email: email,
+ profile_picture : imageUrl
+ });
+ }
+ */
