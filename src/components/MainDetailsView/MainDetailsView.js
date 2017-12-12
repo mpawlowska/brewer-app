93921283932caa0cafc0b48c55d499744de16a22
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2017-12-12 16:27:01 +0100

    New files structure of AddBatchView

--- src/components/MainDetailsView/MainDetailsView.js
+++ src/components/MainDetailsView/MainDetailsView.js
@@ -10,11 +10,26 @@ export default class MainDetailsView extends React.Component {
 
         this.state = {
             batches: []
-        }
+        };
     }
 
-    componentWillMount() {
-        const batchesRef = firebase.database().ref().orderByKey();
+    // tym sposobem wyświetla się tylko jedna warka - dlaczego? ale wyswietla się od razu, bez koniecnzości klikania na 'Karty'
+
+    // onDataChange = (snap) => {
+    //     let batches = [];
+    //
+    //     snap.forEach(childSnap => batches.push(childSnap.val()));
+    //
+    //     this.setState({batches: batches})
+    // };
+    //
+    // componentDidMount() {
+    //     const batchesRef = firebase.database().ref();
+    //     batchesRef.on('value', this.onDataChange);
+    // }
+
+    componentDidMount() {
+        const batchesRef = firebase.database().ref();
         let batches = [];
 
             batchesRef.on('value', snap => {
@@ -22,6 +37,7 @@ export default class MainDetailsView extends React.Component {
                     batches.push(childSnapshot.val());
                 })
             });
+
             this.setState({batches: batches});
         }
 
