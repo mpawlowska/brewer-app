365a0ea18a4acf8c8f9805c395432fb293ea41dc
Magdalena Pawłowska <mag.pawloowska@gmail.com>
2017-12-17 10:11:02 +0100

    update app to use HashRouter instead of BrowserRouter (hosting purposes)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'normalize.css';
 import Header from '../Header/Header.js';
 import MainActionButtons from '../MainActionButtons/MainActionButtons.js'
